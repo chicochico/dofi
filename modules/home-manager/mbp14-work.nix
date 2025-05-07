5bb2663fc21fc72c0cdc4804c200f1fcc9ef25c9
Francisco Chiang <mail@fchiang.com>
2025-05-08 00:09:02 +0200

    Add multi-gitter

--- modules/home-manager/mbp14-work.nix
+++ modules/home-manager/mbp14-work.nix
@@ -10,6 +10,19 @@ in
     username = "franciscochiang";
     packages = with pkgs; common.home.packages ++ [
       glab
+      (pkgs.stdenv.mkDerivation {
+        pname = "multi-gitter";
+        version = "0.57.1";
+        src = pkgs.fetchurl {
+          url = "https://github.com/lindell/multi-gitter/releases/download/v0.57.1/multi-gitter_0.57.1_Darwin_ARM64.tar.gz";
+          sha256 = "7b40579e264d1d28251de329bd031feff0754de76ca7fd936a3774b891e41091";
+        };
+        installPhase = ''
+          tar -xzvf $src
+          mkdir -p $out/bin
+          cp multi-gitter $out/bin/
+        '';
+      })
     ];
 
     file = common.home.file // {
